61c9888c55c401cea4cbaa9ba7e12d2b1ed6105c
PedroReves <pedroreves11@gmail.com>
2024-01-24 00:15:55 -0300

    :lipstick: feat(lint): Linting Project and Final Back-End Construction

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,3 +1,4 @@
+import { useForm } from 'react-hook-form'
 import { Button } from './ui/button'
 import {
   Card,
@@ -8,12 +9,49 @@ import {
 } from './ui/card'
 import { Input } from './ui/input'
 import { Label } from './ui/label'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const createUserSchema = z
+  .object({
+    name: z
+      .string()
+      .min(3, { message: 'Names needs to be more than 3 characters' })
+      .regex(/[a-zA-Z\u00C0-\u00FF ]+/i, {
+        message: 'Your name cant have numbers',
+      }),
+    email: z
+      .string()
+      .email({ message: 'This Input is not an valid email' })
+      .regex(
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+      ),
+    password: z
+      .string()
+      .min(8, { message: 'Your password must have 8 characters ' }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: 'Your password must have 8 characters ' }),
+  })
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    message: 'The password does not match',
+    path: ['confirmPassword'],
+  })
+
+type createUserFormData = z.infer<typeof createUserSchema>
+
+function handleFormSubmit(data: createUserFormData): void {
+  console.table(data)
+}
 
 export default function Form() {
-  /* function handleFormSubmit(data: unknown): void {
-    console.log(data)
-  } */
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<createUserFormData>({
+    resolver: zodResolver(createUserSchema),
+  })
   return (
     <Card className="w-1/3">
       <CardHeader>
@@ -27,23 +65,53 @@ export default function Form() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
           <div className="flex flex-col gap-y-4">
             <div>
               <Label htmlFor="name">Name: </Label>
-              <Input type="text" placeholder="Enter your name..." />
+              <Input
+                type="text"
+                placeholder="Enter your name..."
+                {...register('name', { required: true })}
+              />
+              {errors.name && (
+                <span className="error-message">{errors.name.message}</span>
+              )}
             </div>
             <div>
               <Label htmlFor="email">Email: </Label>
-              <Input type="email" placeholder="Enter your email..." />
+              <Input
+                type="email"
+                placeholder="Enter your email..."
+                {...register('email', { required: true })}
+              />
+              {errors.email && (
+                <span className="error-message">{errors.email.message}</span>
+              )}
             </div>
             <div>
-              <Label htmlFor="password">Name: </Label>
-              <Input type="password" placeholder="Enter your password..." />
+              <Label htmlFor="password">Password: </Label>
+              <Input
+                type="password"
+                placeholder="Enter your password..."
+                {...register('password', { required: true })}
+              />
+              {errors.password && (
+                <span className="error-message">{errors.password.message}</span>
+              )}
             </div>
             <div>
               <Label htmlFor="confirmPassword">Confirm Password: </Label>
-              <Input type="password" placeholder="Confirm your password..." />
+              <Input
+                type="password"
+                placeholder="Confirm your password..."
+                {...register('confirmPassword', { required: true })}
+              />
+              {errors.confirmPassword && (
+                <span className="error-message">
+                  {errors.confirmPassword.message}
+                </span>
+              )}
             </div>
             <Button type="submit" className="mt-4">
               Sign In
